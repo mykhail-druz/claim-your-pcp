4f48d5004d5340da341c23fd90096f02128f9790
Danylo Kachanko <d.kachanko@gmail.com>
2024-04-02 18:36:35 +0300

    Fix

--- src/components/VehicleRegistration/FirstStep/FirstStepSearch.tsx
+++ src/components/VehicleRegistration/FirstStep/FirstStepSearch.tsx
@@ -13,10 +13,10 @@ export const FirstStepSearch: React.FC<RegisterProps> = ({
   skipRegistration,
 }) => {
   const [isChecked, setIsChecked] = useState(false);
-  const [vehicleData, setVehicleData] = useState<any>(null); 
-  const [error, setError] = useState<string | null>(null); 
+  const [vehicleData, setVehicleData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
   const [inputValue, setInputValue] = useState("");
-  
+
   const toggleCheckbox = () => {
     setIsChecked(!isChecked);
   };
@@ -160,26 +160,27 @@ export const FirstStepSearch: React.FC<RegisterProps> = ({
             minLength: 4,
             maxLength: 8,
           })}
-          onChange={(e)=>handleInputChange(e)}
+          onChange={(e) => handleInputChange(e)}
         />
-        <button
-          disabled={!inputValue.trim()}
-          onClick={handleSearch}
+        <a
+          // type="button"
+          // disabled={!inputValue.trim()}
+          onClick={()=>handleSearch()}
           className={`${styles.input_button} ${roobertMedium.className}`}
         >
           Search
-        </button>
+        </a>
       </div>
-      <button className={`${styles.mobile_button}`}>Search</button>
+      <a onClick={() => handleSearch()} className={`${styles.mobile_button}`}>Search</a>
       {/* {error && <p className="text-red-500">{error}</p>} */}
       <div className="flex space-x-2 items-center">
         <label className="switch">
           <input
             type="checkbox"
             checked={isChecked}
-           
+
             {...register("privateReg")}
-            onChange={()=>toggleCheckbox()}
+            onChange={() => toggleCheckbox()}
           />
           <span className="slider"></span>
         </label>
@@ -191,12 +192,12 @@ export const FirstStepSearch: React.FC<RegisterProps> = ({
         </a>
       </div>
       <div className="pt-4">
-      <a
-        onClick={()=>skipRegistration()}
-        className={`${styles.skip} ${roobertMedium.className}`}
-      >
-        Skip Registration Plate
-      </a>
+        <a
+          onClick={() => skipRegistration()}
+          className={`${styles.skip} ${roobertMedium.className}`}
+        >
+          Skip Registration Plate
+        </a>
       </div>
     </div>
   );
